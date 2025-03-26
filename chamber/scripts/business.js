e12538015be637b4e79055b405b63a2648b82212
laisadubiela <laisa.sf@hotmail.com>
2025-03-26 00:36:17 -0300

    only need the list for 320px

--- chamber/scripts/business.js
+++ chamber/scripts/business.js
@@ -3,20 +3,26 @@ const membersData = 'data/members.json';
 const container = document.querySelector('.container');
 const gridButton = document.querySelector('#grid-button');
 const listButton = document.querySelector('#list-button');
+let switchButton = "grid"
 
 gridButton.addEventListener('click', () => {
-  gridDisplayMembers(data.members);
+  switchButton = "grid"
+  container.style.display = "grid";
+  getMembersData();
 });
 
 listButton.addEventListener('click', () => {
-  listDisplayMembers(data.members);
+  switchButton = "list"
+  container.style.display = "flex";
+  getMembersData();
 });
 
 async function getMembersData() {
     const response = await fetch(membersData);
     const data = await response.json();
     console.table(data.members);
-    gridDisplayMembers(data.members);
+    if(switchButton === "grid") gridDisplayMembers(data.members);
+    else listDisplayMembers(data.members);
   }
   
   getMembersData();
@@ -51,29 +57,49 @@ async function getMembersData() {
 
   const listDisplayMembers = (members) => {
     container.innerHTML = "";
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tr = document.createElement('tr');
+    let th1 = document.createElement('th');
+    let th2 = document.createElement('th');
+    let th3 = document.createElement('th');
+    let th4 = document.createElement('th');
+    let tbody = document.createElement('tbody');
+
+    th1.textContent = 'Name';
+    th2.textContent = 'Address';
+    th3.textContent = 'Phone';
+    th4.textContent = 'Website';
+
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+    tr.appendChild(th4);
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
     members.forEach((member) => {
       
-      let name = document.createElement('name');
-      let address = document.createElement('p');
-      let phone = document.createElement('p');
-      let website = document.createElement('a');
+      let tr = document.createElement('tr');
+      let name = document.createElement('td');
+      let address = document.createElement('td');
+      let phone = document.createElement('td');
+      let website = document.createElement('td');
+      let websiteLink = document.createElement('a');
   
       name.textContent = member.name;
       address.textContent = member.address;
       phone.textContent = member.phone;
-      website.textContent = member.website;
+      websiteLink.textContent = member.website;
+      websiteLink.setAttribute('href', member.website);
 
-      website.setAttribute('href', member.website);
-      name.setAttribute('src', `images/${member.image}`);	
-      name.setAttribute('alt', member.name); 
-      name.setAttribute('loading', 'lazy');
-      card.classList.add('card');
-  
-      card.appendChild(name); 
-      card.appendChild(address);
-      card.appendChild(phone);
-      card.appendChild(website);
-  
-      container.appendChild(card);
-    }); 
+      website.appendChild(websiteLink);
+      tr.appendChild(name); 
+      tr.appendChild(address);
+      tr.appendChild(phone);
+      tr.appendChild(website);
+      tbody.appendChild(tr);
+    });
+    table.appendChild(tbody);
+    container.appendChild(table);
   }

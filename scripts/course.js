c19cd9deb8473ba33079942ef0f73225a10e3d7f
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-04-10 16:08:50 -0300

    Update course.js

--- scripts/course.js
+++ scripts/course.js
@@ -128,3 +128,25 @@ document.getElementById("WDD").addEventListener("click", () => filterCourses("WD
 document.getElementById("CSE").addEventListener("click", () => filterCourses("CSE"));
 
 renderCourses(courses);
+
+function displayCourseDetails(course) {
+  courseDetails.innerHTML = '';
+  courseDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits</strong>: ${course.credits}</p>
+    <p><strong>Certificate</strong>: ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+  `;
+  courseDetails.showModal();
+  
+  closeModal.addEventListener("click", () => {
+    courseDetails.close();
+  });
+}
+
+courseDiv.addEventListener('click', () => {
+      displayCourseDetails(course);
+    });
